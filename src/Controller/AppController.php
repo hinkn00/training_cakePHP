422a70e221d6455de65188edae5b2b9ebcff9945
NGUYEN THANH TUNG <tungnt@OMINEXT.LOCAL>
2022-01-17 16:54:01 +0700

    fix src/Controller/AppController

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -79,6 +79,8 @@ class AppController extends Controller
 
     public function beforeFilter(EventInterface $event) {
         parent::beforeFilter($event);
-        $this->Auth->allow(['add', 'verification', 'logout','forgotPass','resetPass']);        
+        // $this->Auth->allow(['add', 'verification', 'logout','forgotPass','resetPass']);
+        $this->Auth->allow();
+
     }
 }
